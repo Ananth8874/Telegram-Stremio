ee9b4b43a0777d1195250dde6d9e5c60b02a7891
Ananth8874 <heart81570801@gmail.com>
2026-04-29 15:47:05 +0530

    Update config.py

--- Backend/config.py
+++ Backend/config.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 load_dotenv(path.join(path.dirname(path.dirname(__file__)), "config.env"))
 
 class Telegram:
-    API_ID = int(getenv("API_ID", "0"))
-    API_HASH = getenv("API_HASH", "")
-    BOT_TOKEN = getenv("BOT_TOKEN", "")
+    API_ID = int(getenv("API_ID", "22977776"))
+    API_HASH = getenv("API_HASH", "2ac7223d720bdeec757cbc88ced57224"")
+    BOT_TOKEN = getenv("BOT_TOKEN", "8224774305:AAHjHNJRMJQnBj8XZUZOcSmqCSzGGehaV8s")
     HELPER_BOT_TOKEN = getenv("HELPER_BOT_TOKEN", "")
 
     BASE_URL = getenv("BASE_URL", "").rstrip('/')
@@ -15,15 +15,15 @@ class Telegram:
     PARALLEL = int(getenv("PARALLEL", "1"))
     PRE_FETCH = int(getenv("PRE_FETCH", "1"))
 
-    AUTH_CHANNEL = [channel.strip() for channel in (getenv("AUTH_CHANNEL") or "").split(",") if channel.strip()]
-    DATABASE = [db.strip() for db in (getenv("DATABASE") or "").split(",") if db.strip()]
+    AUTH_CHANNEL = [channel.strip() for channel in (getenv("AUTH_CHANNEL") or "-1003974332689").split(",") if channel.strip()]
+    DATABASE = [db.strip() for db in (getenv("DATABASE") or "mongodb+srv://jeevanantham8157:1055221@tamizhzone.b0bx6bx.mongodb.net/?appName=TamizhZone").split(",") if db.strip()]
 
-    TMDB_API = getenv("TMDB_API", "")
+    TMDB_API = getenv("TMDB_API", "57d8345a6dd40ec2ca3cc43ba707457c")
 
     UPSTREAM_REPO = getenv("UPSTREAM_REPO", "")
     UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "")
 
-    OWNER_ID = int(getenv("OWNER_ID", "5422223708"))
+    OWNER_ID = int(getenv("OWNER_ID", "8537200428"))
     
     REPLACE_MODE = getenv("REPLACE_MODE", "true").lower() == "true"
     HIDE_CATALOG = getenv("HIDE_CATALOG", "false").lower() == "true"
